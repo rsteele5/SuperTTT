57ba605fc17fd6cdb9fbf3f9242a9e87a7bcbfbc
CoreyNE <cnelton@outlook.com>
2017-04-25 14:26:57 -0500

    AIO client/server uploaded

---
+++ TestAI2 (AIO)/src/testai2/TestAI2.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testai2;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author cnelt
+ */
+public class TestAI2 {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) 
+        throws IOException {
+            AI myAI = new AI();
+            int received = -1, sent;
+            String aiMove;
+
+            if(received == -1){
+                 ServerTest2 server = new ServerTest2(); //create server with move we will make
+                 for(int plays = 0; plays < 12; plays++){
+                    System.out.println("Move recieved: " + received);
+                    sent = myAI.makeMove(received);//call AI and make move
+                    System.out.println("Move played: " + sent);
+
+                    server.send(sent); //send that move and wait for response
+                    server.receive();
+                    received = server.received; //retrives the number sent from enemy
+                }
+            }else{
+                ClientTest2 client = new ClientTest2("10.0.2.15");
+                for(int plays = 0; plays < 12; plays++){
+                    int recieved;
+                    client.receive();
+                    recieved = client.number;
+
+                    System.out.println("move recieved: " + recieved);
+                    sent = myAI.makeMove(recieved);
+                    System.out.println("Move played: " + sent);
+
+                    client.send(sent); //send AI move
+                }
+            }
+            //if playing first
+
+            //if playing second
+
+
+    }
+    
+}
