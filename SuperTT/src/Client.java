d543c54faaf68a156414554da8b953bd0b516743
CoreyNE <cnelton@outlook.com>
2017-04-25 22:43:08 -0500

    fixed client/server return

--- SuperTT/src/Client.java
+++ SuperTT/src/Client.java
@@ -17,43 +17,27 @@
      public static int receive()
      throws IOException
        {
-//         String host;          
-//         host = "10.32.38.32";
-//         Socket sock = null;
          try
            {
-             // make connection to a socket
-//             sock = new Socket(host, 5195);  // socket for contact with server
-
              DataInputStream  dis   = new DataInputStream(sock.getInputStream());     // for reading ints
-             return number = dis.readInt(); //gets integer from talker  
-			
+             return number = dis.readInt(); //gets integer from talker  			
             //sock.setSoTimeout(5000);
            
            }
          catch (IOException ioe)
            {
              System.err.println(ioe);
-           }
-    
+             return 25;
+           }    
        }
      public static void send(int aiMove)throws IOException
        {
-//         String host;          
-//         host = "10.32.38.32";
-//         Socket sock = null;
          try
            {
-             // make connection to a socket
-//             sock = new Socket(host, 5195);  // socket for contact with server
-
              DataOutputStream out = new DataOutputStream(sock.getOutputStream());
-             
              out.writeInt(aiMove); //sends AI's move to enemy
-//             out.close();
-			
+
             //sock.setSoTimeout(5000);
-           
            }
          catch (IOException ioe)
            {
--- SuperTT/src/Server.java
+++ SuperTT/src/Server.java
@@ -24,24 +24,14 @@ import java.io.*;
 //         ServerSocket sock = null; // holds the server socket
          try
            {
-//            sock = new ServerSocket(5195);    // Port for the server socket
-//            client = sock.accept( );  //new computer we are talking to
-//            DataOutputStream out = new DataOutputStream(client.getOutputStream());
-            DataInputStream  in   = new DataInputStream(client.getInputStream());     // for reading bytes
-//            out.writeInt(send); //sends AI's move to enemy
-//            out.close(); //close the data outstream
-            
+            DataInputStream  in   = new DataInputStream(client.getInputStream());     // for reading integer            
             return received = in.readInt();//waits here until an integer is read in
            }
          catch (IOException ioe)
            {
              System.err.println(ioe);
+             return 25;
            }
-//         finally
-//           {
-//             if (client != null) client.close();
-//             if (sock != null) sock.close();
-//           }
        }
      
      public static void send(int x)
