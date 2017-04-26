13cbf0585e97d3f4909e32dd1824136c5b916e7c
FarzanKalantari <fkalantari@uco.edu>
2017-04-26 06:12:39 -0500

    Merge branch 'Farzan'
    # Conflicts:
    #	TestAI/build/classes/testai/AI.class
    #	TestAI/build/classes/testai/TestAI.class

--- SuperTT/src/Client.java
+++ SuperTT/src/Client.java
@@ -5,10 +5,11 @@
    {
      static int number;
      static Socket sock;
+     String myIP = GetAddr.IP;
 //     static int send;
-     Client(String host)throws IOException{
+     Client()throws IOException{
          try{
-             sock = new Socket(host, 5195);
+             sock = new Socket(myIP, 5195);
          }
          catch(IOException ioe){
              System.err.println(ioe);
