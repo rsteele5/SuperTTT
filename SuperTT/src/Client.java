d301129cc2a586086806800173ce9bb5ba7f3803
FarzanKalantari <fkalantari@uco.edu>
2017-04-26 02:17:14 -0500

    Merge remote-tracking branch 'origin/master' into Farzan

--- SuperTT/src/Client.java
+++ SuperTT/src/Client.java
@@ -4,12 +4,12 @@
  public class Client
    {
      static int number;
-     static Socket sock;   
+     static Socket sock;   // holds the server socket
      String myIP = GetAddr.IP;
 //     static int send;
      Client()throws IOException{
          try{
-             sock = new Socket(myIP, 5195);
+             sock = new Socket(myIP, 5195); //sets up socket with ip from input on port number
          }
          catch(IOException ioe){
              System.err.println(ioe);
@@ -22,12 +22,13 @@
            {
              DataInputStream  dis   = new DataInputStream(sock.getInputStream());     // for reading ints
              number = dis.readInt(); //gets integer from talker  			
-             sock.setSoTimeout(5000);
+             sock.setSoTimeout(10000); //connection times out after 10 seconds
              return number;
            }
          catch (IOException ioe)
            {
              System.err.println(ioe);
+             System.err.println("Opponent timed out. ");
              return 999;
            }    
        }
@@ -35,7 +36,7 @@
        {
          try
            {
-             DataOutputStream out = new DataOutputStream(sock.getOutputStream());
+             DataOutputStream out = new DataOutputStream(sock.getOutputStream());  //for writing ints
              out.writeInt(aiMove); //sends AI's move to enemy
 
             //sock.setSoTimeout(5000);
