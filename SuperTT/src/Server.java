d15483346374cbd2d6a9ee6ab78fdc6df6538d9e
CoreyNE <cnelton@outlook.com>
2017-04-25 21:40:50 -0500

    Added server/client to supertt

---
+++ SuperTT/src/Server.java
@@ -0,0 +1,78 @@
+import java.io.*;
+ import java.net.*;
+
+ public class Server
+   {
+     static int received;
+     static int send;
+     static ServerSocket sock;
+     static Socket client;
+     Server() throws IOException{
+         try{
+             sock = new ServerSocket(5195);
+             client = sock.accept();
+         }
+         catch(IOException ioe){
+              System.err.println(ioe);
+         }
+     }
+
+     public static void receive()
+     throws IOException
+       {
+//         Socket client = null;     // holds the socket for the client
+//         ServerSocket sock = null; // holds the server socket
+         try
+           {
+//            sock = new ServerSocket(5195);    // Port for the server socket
+//            client = sock.accept( );  //new computer we are talking to
+//            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            DataInputStream  in   = new DataInputStream(client.getInputStream());     // for reading bytes
+//            out.writeInt(send); //sends AI's move to enemy
+//            out.close(); //close the data outstream
+            
+            received = in.readInt();//waits here until an integer is read in
+           }
+         catch (IOException ioe)
+           {
+             System.err.println(ioe);
+           }
+//         finally
+//           {
+//             if (client != null) client.close();
+//             if (sock != null) sock.close();
+//           }
+       }
+     
+     public static void send(int x)
+     throws IOException
+       {
+//         Socket client = null;     // holds the socket for the client
+//         ServerSocket sock = null; // holds the server socket
+         send = x;
+         try
+           {
+//            sock = new ServerSocket(5195);    // Port for the server socket
+//            client = sock.accept( );  //new computer we are talking to
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+//            DataInputStream  in   = new DataInputStream(client.getInputStream());     // for reading bytes
+            out.writeInt(send); //sends AI's move to enemy
+//            out.close(); //close the data outstream
+            
+//            received = in.readInt();//waits here until an integer is read in
+           }
+         catch (IOException ioe)
+           {
+             System.err.println(ioe);
+           }
+//         finally
+//           {
+//             if (client != null) client.close();
+//             if (sock != null) sock.close();
+//           }
+       }
+ 
+   }
+
+
+ 
